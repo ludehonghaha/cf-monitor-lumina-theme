59b2434e11f5e641fd58b82f5a346b18329eef48
hahadh <hahadhlu@gmail.com>
2026-07-17 18:28:40 +0800

    Update config.js
    增加服务器图片

--- config.js
+++ config.js
@@ -19,13 +19,51 @@ window.LUMINA_CONFIG = {
    * 服务器名称必须与监控面板显示名称完全一致
    */
   serverImages: {
-    "Akile-JP": {
-      src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/jp.jpg",
-      alt: "Akile-JP",
-      opacity: 0.48,
-      height: 120,
-      fit: "cover",
-      position: "center 45%"
-    }
+  "dedirock-US": {
+    src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/dedirock-us.jpg",
+    alt: "dedirock-US",
+    opacity: 0.55,
+    fit: "cover",
+    position: "center"
+  },
+
+  "dedione-US": {
+    src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/dedione-us.jpg",
+    alt: "dedione-US",
+    opacity: 0.55,
+    fit: "cover",
+    position: "center"
+  },
+
+  "zouter-HK": {
+    src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/zouter-hk.jpg",
+    alt: "zouter-HK",
+    opacity: 0.55,
+    fit: "cover",
+    position: "center"
+  },
+
+  "Akile-JP": {
+    src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/jp.jpg",
+    alt: "Akile-JP",
+    opacity: 0.55,
+    fit: "cover",
+    position: "center 45%"
+  },
+
+  "datewave-tw": {
+    src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/datewave-tw.jpg",
+    alt: "datewave-tw",
+    opacity: 0.55,
+    fit: "cover",
+    position: "center"
+  },
+
+  "Lightlayer-US": {
+    src: "https://cdn.jsdelivr.net/gh/ludehonghaha/cf-monitor-lumina-theme@main/assets/lightlayer-us.jpg",
+    alt: "Lightlayer-US",
+    opacity: 0.55,
+    fit: "cover",
+    position: "center"
   }
-};
+}
